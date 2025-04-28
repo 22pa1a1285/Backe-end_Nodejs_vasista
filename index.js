454807f4019f92035f81c34c8a47087108f78653
22pa1a1285 <22pa1a1285@vishnu.edu.in>
2025-04-28 23:43:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
- const dotEnv = require("dotenv");
- dotEnv.config(); 
+const dotEnv = require("dotenv");
+dotEnv.config(); 
 
 const mongoose = require("mongoose");
 const app = express();
@@ -11,25 +11,24 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((error)=>console.log(error));
 
 const vendorRoutes = require('./routes/vendorRoutes');
-const bodyparser = require("body-parser");
 const firmRoutes = require("./routes/firmRoutes");
 const productRoutes = require("./routes/productRoutes");
+const bodyparser = require("body-parser");
 const path = require('path');
 
 app.use(bodyparser.json());
 
-
-
-app.use('/vendor',vendorRoutes);
-app.use("/firm",firmRoutes);
-app.use("/product",productRoutes);
-app.use('/uploads',express.static('uploads'));
-
-app.listen(PORT,()=>{
-    console.log(`server started and running successfully at ${PORT}`);
+// 🟰 define welcome route here
+app.get('/', (req, res) => {
+    res.send("<h1>welcome to vasista</h1>");
 });
 
+// then define your APIs
+app.use('/vendor', vendorRoutes);
+app.use("/firm", firmRoutes);
+app.use("/product", productRoutes);
+app.use('/uploads', express.static('uploads'));
 
-app.use('/',(req,res)=>{
-    res.send("<h1>welcome to vasista");
-})
+app.listen(PORT, () => {
+    console.log(`server started and running successfully at ${PORT}`);
+});
